cb238de2416285e368b0f5caa2b274a0b7f44f33
science-research-edu <ek2012mayday@gmail.com>
2026-02-25 21:57:43 -0500

    frcwork9

--- webapp/patch.js
+++ webapp/patch.js
@@ -1,44 +1,25 @@
-console.log("Super-Patch.js: GitHub Pathing Active.");
+console.log("Super-Patch.js: GitHub Redirect Active.");
 
-// FORCE ENGINE PATHS
-// This intercepts every request the game makes and adds the folder name
 const originalOpen = XMLHttpRequest.prototype.open;
 XMLHttpRequest.prototype.open = function(method, url) {
-    let finalUrl = url;
-
-    // If it's looking for engine files, force them into the webapp folder
+    // FORCE REDIRECT: If looking for engine data at the root, point to webapp/
     if (url === "index_stripped.data" || url === "index_stripped.wasm") {
-        finalUrl = "webapp/" + url;
+        url = "webapp/" + url;
+        console.log("Patch: Redirecting engine data to: " + url);
     }
-
-    // If it's looking for the game data file
+    
+    // Fix for the game data itself
     if (url.includes("6208082864562F76.data")) {
-        finalUrl = "./6208082864562F76.data";
+        url = "./6208082864562F76.data";
     }
 
-    // Ghost Server for API calls
-    if (url.includes(":5006") || url.includes("/ledger") || url.includes("/user")) {
-        this._isGhost = true;
-    }
-
-    return originalOpen.apply(this, [method, finalUrl]);
-};
-
-const originalSend = XMLHttpRequest.prototype.send;
-XMLHttpRequest.prototype.send = function() {
-    if (this._isGhost) {
-        Object.defineProperty(this, 'status', { writable: false, value: 200 });
-        Object.defineProperty(this, 'readyState', { writable: false, value: 4 });
-        Object.defineProperty(this, 'responseText', { writable: false, value: '{"success":true}' });
-        this.dispatchEvent(new Event('load'));
-        return;
-    }
-    return originalSend.apply(this, arguments);
+    this._url = url;
+    return originalOpen.apply(this, arguments);
 };
 
 // Data Downloader
 window.downloadLinkedGame = function(guid) {
-    console.log("Patch: Fetching " + guid);
+    console.log("Patch: Manually fetching " + guid);
     fetch("./" + guid + ".data")
         .then(r => r.blob())
         .then(blob => {
